0dfcb659c0e703178a1847b489c3794cc90b10b3
lulubun <laurapetitt@gmail.com>
2018-12-11 21:04:41 -0500

    chapter 2

---
+++ src/components/Dog.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card color="grey lighten-2">
+    <v-img :src="dog.img" height="170px"> </v-img>
+    <v-card-title>
+      <div>
+        <h3>{{dog.name}}</h3>
+        <h3>{{dog.breed}}</h3>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+	    dog: {
+	      type: Object
+	    }
+	  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify';
+import VueRouter from 'vue-router';
 import 'vuetify/dist/vuetify.min.css';
 
+import Home from './views/Home';
+import Pets from './views/Pets';
+
 Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/pets', component: Pets },
+];
+
+const router = new VueRouter({ routes });
 
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>',
+  router,
 })
